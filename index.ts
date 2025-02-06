483b904e7aa91f1e1034abd5ed19208394c7bfb1
unibox <unibox@duck.com>
2025-02-06 10:53:04 +0100

    add urlencoded

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (_, res) => {
